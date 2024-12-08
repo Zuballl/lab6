c12573e41c9a836a9c681d5f1ff6be2f0f1ba9f0
Zuballl <115400165+Zuballl@users.noreply.github.com>
2024-12-08 20:06:56 +0100

    first commit

--- obj/Debug/net8.0/RealTimeSystem.AssemblyInfo.cs
+++ obj/Debug/net8.0/RealTimeSystem.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RealTimeSystem")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8c62202c4903fcbc01e673e6a9f5c99a5cef6528")]
 [assembly: System.Reflection.AssemblyProductAttribute("RealTimeSystem")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RealTimeSystem")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
